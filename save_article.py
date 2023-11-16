fb669eac5d3d324ef36c1fde0a6febf00b7c713a
Deng Seng Ang <dengseng.dev@gmail.com>
2023-11-16 17:10:49 +0800

    Update save_article.py

--- save_article.py
+++ save_article.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def get_html_content(url):
     try:
@@ -26,3 +27,23 @@ if html_content:
     save_html_to_file(html_content, file_path)
 else:
     print("Failed to retrieve HTML content from the URL.")
+
+
+def extract_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Extract the main content with a space as separator
+    main_content = soup.get_text(separator=' ', strip=True)
+
+    return main_content
+
+if html_content:
+    article_content = extract_content(html_content)
+
+    if article_content:
+        # Display the extracted article content
+        print(article_content)
+    else:
+        print("Failed to extract content from the HTML file.")
+else:
+    print("Failed to read HTML content from the file.")
